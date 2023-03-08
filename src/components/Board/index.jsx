a6a0130d94935270b5fcebb3f075e6bdf28692f3
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-03-08 15:53:55 +0100

    Pintado el tablero

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,38 +1,108 @@
 import './index.css'
 
 export const Board = () => {
-  const board = new Array(64).fill(0)
-
   return (
     <div className='board-container'>
 
-      <div className='board-letter-row'>
-        <div className='board-field letter'>A</div>
-        <div className='board-field letter'>B</div>
-        <div className='board-field letter'>C</div>
-        <div className='board-field letter'>D</div>
-        <div className='board-field letter'>E</div>
-        <div className='board-field letter'>F</div>
-        <div className='board-field letter'>G</div>
-        <div className='board-field letter'>H</div>
-      </div>
-      <div className='board-number-column'>
-        <div className='board-field number'>1</div>
-        <div className='board-field number'>2</div>
-        <div className='board-field number'>3</div>
-        <div className='board-field number'>4</div>
-        <div className='board-field number'>5</div>
-        <div className='board-field number'>6</div>
-        <div className='board-field number'>7</div>
-        <div className='board-field number'>8</div>
-      </div>
-      <div className='board'>
-        {
-        board.map((value, index) => {
-          return <div key={'board-field' + index} className='board-field' />
-        })
-      }
-      </div>
+      <table className='board-game'>
+        <tr>
+          <td className='box'>
+            <span className='info letter'>A</span>
+            <span className='info number'>1</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>B</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>C</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>D</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>E</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>F</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>G</span>
+          </td>
+          <td className='box'>
+            <span className='info letter'>H</span>
+          </td>
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>2</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>3</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>4</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>5</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>6</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>7</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+        <tr>
+          <td className='box'><span className='info number'>8</span></td>
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+          <td className='box' />
+        </tr>
+      </table>
     </div>
   )
 }
